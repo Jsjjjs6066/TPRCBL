dfe6448fbba2ba227908f4ac8a422752f4035787
Jsjjjs6066 <jsjjjs6066@yahoo.com>
2024-05-31 14:47:16 +0200

    idk

--- TPRCBL/__init__.py
+++ TPRCBL/__init__.py
@@ -1,10 +1,14 @@
-from sys import argv, stdin
+from sys import argv
+from os import get_terminal_size
+from msvcrt import getwch
 
 import json
 
 from render import render
+from render.title import setTitle
 from elements import *
 
+setTitle('Loading from file...')
 class ParsedPage:
     def __init__(self, title: str = 'Page', elenents: list[Element] = []) -> None:
         self.title = title
@@ -17,6 +21,13 @@ class Page:
         return parse(self)
 
 def parse(code: Page) -> ParsedPage:
+    elems: list[Element] = []
+    for i in code.code.get('body'):
+        if i[0] == 'para':
+            if len(i) == 2:
+                elems.append(Para(i[1]))
+            else:
+                elems.append(Para(i[1]), i[2])
     return ParsedPage(code.code.get('title'))
 
 def file(path: str) -> Page:
@@ -24,14 +35,24 @@ def file(path: str) -> Page:
         d: str = f.read()
         return Page(json.loads(d))
 
+def load():
+    page: Page = file(argv[1])
+    setTitle('Parsing loaded file...')
+    decoded: ParsedPage = page.decode()
 
-page: Page = file(argv[1])
-decoded: ParsedPage = page.decode()
+    setTitle('Rendering loaded parsed file...')
+    render(decoded)
 
-render(decoded)
-
-try:
-    while True: 
-        stdin.readline()
-except KeyboardInterrupt:
-    print('Exited TPRCBL')
+if __name__ == '__main__':
+    load()
+    try:
+        while True: 
+            inp: str = getwch()
+            if inp.lower() == 'x':
+                print('Exited TPRCBL', end='')
+                exit()
+            if inp.lower() == 'r':
+                print('\n' * get_terminal_size().lines)
+                load()
+    except KeyboardInterrupt:
+        print('Exited TPRCBL', end='')
--- TPRCBL/elements/__init__.py
+++ TPRCBL/elements/__init__.py
@@ -4,3 +4,10 @@ class Element:
         self.args = args
     def render():
         pass
+
+class Para(Element):
+    def __init__(self, innerTPRCBL: dict | str | None = None, args: dict = {}) -> None:
+        super().__init__(innerTPRCBL, args)
+    def render(self):
+        if type(self.innerTPRCBL) == type(str):
+            print(self.innerTPRCBL)
