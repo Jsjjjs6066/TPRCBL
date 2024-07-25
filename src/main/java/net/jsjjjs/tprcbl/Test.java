869fec19275a2058503c201ed529cb2e90797318
Jsjjjs6066 <jsjjjs6066@outlook.com>
2024-07-25 12:41:27 +0200

    new stuff

--- src/main/java/net/jsjjjs/tprcbl/Test.java
+++ src/main/java/net/jsjjjs/tprcbl/Test.java
@@ -1,13 +1,22 @@
 package net.jsjjjs.tprcbl;
 
 import net.jsjjjs.tprcbl.registries.FGRegistry;
+import org.fusesource.jansi.Ansi;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(FGRegistry.RED.color() + "Interrupted!" + FGRegistry.RESET.color());
-        System.out.println(FGRegistry.YELLOW.color() + "Get warned!" + FGRegistry.RESET.color());
-        System.out.println(FGRegistry.GREEN.color() + "Actually, you are alright!" + FGRegistry.RESET.color());
-        System.out.println("Bye!");
+//        System.out.println(FGRegistry.RED.color() + "Interrupted!" + FGRegistry.RESET.color());
+//        System.out.println(FGRegistry.YELLOW.color() + "Get warned!" + FGRegistry.RESET.color());
+//        System.out.println(FGRegistry.GREEN.color() + "Actually, you are alright!" + FGRegistry.RESET.color());
+//        System.out.println("Bye!");
+        System.out.println(Ansi.ansi().fg(Ansi.Color.BLUE).a("(i) info"));
+        System.out.println(Ansi.ansi().fgBright(Ansi.Color.BLUE).a("(i) info"));
+        System.out.println(Ansi.ansi().fg(Ansi.Color.WHITE).a("> Test"));
+        System.out.println(Ansi.ansi().fgBright(Ansi.Color.WHITE).a("> test"));
+        System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a("(i) Critical"));
+        System.out.println(Ansi.ansi().fgBright(Ansi.Color.RED).a("(i) info"));
+        System.out.println(Ansi.ansi().fg(Ansi.Color.YELLOW).a("/!\\ Warning!"));
+        System.out.println(Ansi.ansi().fgBright(Ansi.Color.YELLOW).a("/!\\ Warning!"));
     }
 }
  
--- src/main/java/net/jsjjjs/tprcbl/logging/Logging.java
+++ src/main/java/net/jsjjjs/tprcbl/logging/Logging.java
@@ -3,10 +3,12 @@ package net.jsjjjs.tprcbl.logging;
 import org.fusesource.jansi.Ansi;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public class Logging {
-    public void log(@NotNull String prefix, Ansi.Color color, Object object) {
-        if (color != null) {
-            System.out.println(Ansi.ansi().fg(color).a(prefix).a(object).fg(Ansi.Color.DEFAULT));
-        }
+    public void log(Ansi.Color color, Object object) {
+        System.out.println(Ansi.ansi().fg(Objects.requireNonNullElse(color, Ansi.Color.DEFAULT)).a(object).fg(Ansi.Color.DEFAULT));
     }
+
+
 }
